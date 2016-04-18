6643f948ba907dda99f058cb5bc4126b6aa54567
fsantanna <francisco.santanna@gmail.com>
2016-04-18 06:47:12 -0300

    (*) first commit

---
+++ cfg/config.lua
@@ -0,0 +1,12 @@
+server = {
+    host = { '127.0.0.1', '8330' },
+    backlog = 128,
+}
+
+client = {
+    peers = {
+        { '127.0.0.1', '8331' },
+        { '127.0.0.1', '8332' },
+    },
+    time_connection_retry = 5000,       -- TODO: 5min?
+}
