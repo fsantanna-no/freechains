cf6e5bbb1eb53315f19053fed1e5a6bb42514048
fsantanna <francisco.santanna@gmail.com>
2016-04-20 22:05:41 -0300

    (>) client sendscomplete transmission (>) server refuse dups

--- src/common.lua
+++ src/common.lua
@@ -55,3 +55,13 @@ function MESSAGE (t)
     end
     APP.messages[#APP.messages+1] = t
 end
+
+function hex_dump(buf)
+  for byte=1, #buf, 16 do
+     local chunk = buf:sub(byte, byte+15)
+     io.write(string.format('%08X  ',byte-1))
+     chunk:gsub('.', function (c) io.write(string.format('%02X ',string.byte(c))) end)
+     io.write(string.rep(' ',3*(16-#chunk)))
+     io.write(' ',chunk:gsub('%c','.'),"\n") 
+  end
+end
