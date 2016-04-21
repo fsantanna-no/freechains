679523ee5a0a80db13af7e6ab458bc38f9f99b2b
fsantanna <francisco.santanna@gmail.com>
2016-04-21 04:52:57 -0300

    (>) read CHAIN_KEY to a string

--- cfg/config-02.lua
+++ cfg/config-02.lua
@@ -7,3 +7,23 @@ CLIENT {
         { '127.0.0.1', '8331' },
     },
 }
+
+-- chains configurations
+local levels = {}
+for i=0, 22 do
+    levels[i] = {
+        head = string.rep('\0',32), -- current chain head
+    }
+end
+
+CHAINS {
+    ['fsantanna'] = {       -- chain id
+        signed = true,      -- messages are signed
+        --signed = false,   -- messages are not signed
+        --mode = 'pub'      -- only publishes messages (storage required)
+        --mode = 'sub'      -- only listen for messages (storeage not required)
+        --mode = 'pub/sub'?
+        [0] = levels[0],
+        unpack(levels),
+    },
+}
---
+++ cfg/message-03.lua
@@ -0,0 +1,12 @@
+MESSAGE {
+    id = '1.0',
+    chain = {
+        --config = nil,
+        key    = 'fsantanna',
+        signed = true,
+        zeros  = 22,
+    },
+    payload = [[
+Francisco Sant'Anna
+]],
+}
