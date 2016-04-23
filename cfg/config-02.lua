14f192042c91c72052df95377ba7ba464bdf9a47
fsantanna <francisco.santanna@gmail.com>
2016-04-23 09:53:49 -0300

    (>) match peers based on chain keys/zeros

--- cfg/config-02.lua
+++ cfg/config-02.lua
@@ -6,6 +6,12 @@ CLIENT {
     peers = {
         {
             host = { '127.0.0.1', '8331' },
+            chains = {
+                {
+                    key = '',
+                    zeros = 0,
+                }
+            },
         },
     },
 }
--- cfg/message-02-01.lua
+++ cfg/message-02-01.lua
@@ -2,7 +2,7 @@ MESSAGE {
     id = '1.0',
     chain = {
         key   = '',
-        zeros = 32,
+        zeros = 30,
     },
     payload = [[
 Heavy message...
