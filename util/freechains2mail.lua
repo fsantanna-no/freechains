4e8748ba59022181d72a0986a790f83c28e1f452
fsantanna <francisco.santanna@gmail.com>
2017-05-02 21:32:37 -0300

    (>) milter: normal, fc, fetchmail, fc2mail

--- util/freechains2mail.lua
+++ util/freechains2mail.lua
@@ -4,11 +4,11 @@ while true do
     local buf do
         local n = assert(tonumber(assert(fin:read('l'))))
         buf = fin:read(n)
-        print'---'
-        print(n)
-        print(buf)
+        --print(n)
+        --print(buf)
     end
 
+    print'=== FC2MAIL'
     local fout = assert(io.popen('mail --subject="Freechains" chico', 'w'))
     fout:write(buf)
     fout:close()
