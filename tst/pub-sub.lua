8951c6423406254913e39d0f631e8211f9dfde2a
fsantanna <francisco.santanna@gmail.com>
2017-10-23 20:47:16 -0200

    (+) removal

---
+++ tst/pub-sub.lua
@@ -0,0 +1,13 @@
+{
+    cmd = 'publish',
+    message = {
+        version = '1.0',
+        chain = {
+            key = '',
+            zeros = 0,
+        },
+        payload = [[
+Subscribe to [oi](freechains:/?cmd=subscribe&key=oi).
+]],
+    },
+}
